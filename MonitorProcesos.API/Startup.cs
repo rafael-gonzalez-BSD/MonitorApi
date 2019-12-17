b59141333378f1b968cfb660ac8b3455837225fe
Rafael <gonzalezrg2@gmail.com>
2019-12-17 10:12:07 -0600

    Se configura swagger

--- MonitorProcesos.API/Startup.cs
+++ MonitorProcesos.API/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace MonitorProcesos.API
 {
@@ -19,6 +20,12 @@ namespace MonitorProcesos.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            // ======== CONFIGURACIÓN DE SWAGGER =========
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "MonitorProcesos", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,6 +35,12 @@ namespace MonitorProcesos.API
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // ======== CONFIGURACIÓN DE SWAGGER =========
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "MonitorProcesos v1");
+            });
 
             app.UseCors(options => options
                 .AllowAnyOrigin()
