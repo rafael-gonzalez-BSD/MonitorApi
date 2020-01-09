c1881f5745881b064274e7bbb91099bec9f97953
Manuel Gilberto León Villegas <manuel.leon@bsdenterprise.com>
2020-01-09 10:49:39 -0600

    Se integro un try catch para controlar errores de ping.

--- MonitorProcesos.API/Controllers/GeneralController.cs
+++ MonitorProcesos.API/Controllers/GeneralController.cs
@@ -12,27 +12,42 @@ namespace MonitorProcesos.API.Controllers
         [HttpGet("testRutaApi")]
         public RespuestaModel TestearRutaApi(string urlApi)
         {
-            RespuestaModel res = new RespuestaModel();
+            RespuestaModel res = new RespuestaModel();            
 
-            Uri uri = new Uri(urlApi);
-            Ping ping = new Ping();
-            PingReply result = ping.Send(uri.Host);
-            if (result.Status == IPStatus.Success)
+            try
             {
-                res.Datos = null;
-                res.ErrorId = 0;
-                res.Id = 0;
-                res.Mensaje = "Ping Satisfactorio";
-                res.Satisfactorio = true;
+                Uri uri = new Uri(urlApi);
+                Ping ping = new Ping();
+                PingReply result = ping.Send(uri.Host);                
+
+                if (result.Status == IPStatus.Success)
+                {
+                    res.Datos = null;
+                    res.ErrorId = 0;
+                    res.Id = 0;
+                    res.Mensaje = "Ping Satisfactorio";
+                    res.Satisfactorio = true;
+                }
+                else
+                {
+                    res.Datos = null;
+                    res.ErrorId = 404;
+                    res.Id = 0;
+                    res.Mensaje = "Error al hacer ping";
+                    res.Satisfactorio = false;
+                }
             }
-            else
+            catch (Exception ex)
             {
+
                 res.Datos = null;
-                res.ErrorId = 404;
+                res.ErrorId = 500;
                 res.Id = 0;
-                res.Mensaje = "Error al hacer ping.";
+                res.Mensaje = "Error interno del sistema. " + ex.Message + ": " + ex.InnerException.ToString();
                 res.Satisfactorio = false;
             }
+
+            
 
             return res;
         }
