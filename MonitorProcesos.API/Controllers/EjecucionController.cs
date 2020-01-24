e9edc8643354cb91149e3aa04a81ed1185229201
Rafael Gonzalez <gonzalezrg2@gmail.com>
2020-01-23 18:39:09 -0600

    Se agrega controller, Negocio, Dao para Ejecuciones

---
+++ MonitorProcesos.API/Controllers/EjecucionController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MonitorProcesos.Entidad.Base;
+using MonitorProcesos.Negocio;
+
+namespace MonitorProcesos.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EjecucionController : ControllerBase
+    {
+        private readonly EjecucionNegocio n;
+
+        public EjecucionController(IConfiguration config)
+        {
+            n = new EjecucionNegocio(config, "MonitorRemoteDev");
+        }
+
+        [HttpGet("grafico")]
+        public async Task<RespuestaModel> ObtenerGraficoSistema(DateTime? FechaOcurrencia, bool? Baja, int Opcion = 5, int EjecucionId = 0, int SistemaId = 0)
+        {
+            FechaOcurrencia = FechaOcurrencia == null ? DateTime.Today : FechaOcurrencia;
+            Dictionary<string, dynamic> param = new Dictionary<string, dynamic>()
+            {
+                {"Opcion", Opcion },
+                {"EjecucionId", EjecucionId },
+                {"FechaOcurrencia", FechaOcurrencia },
+                {"Baja", Baja },
+                {"sistemaId", SistemaId }
+            };
+            return await n.ObtenerGraficoEjecuciones(param);
+        }
+    }
+}
---
+++ MonitorProcesos.Negocio/EjecucionNegocio.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Configuration;
+using MonitorProcesos.Datos.Implementacion;
+using MonitorProcesos.Entidad.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonitorProcesos.Negocio
+{
+    public class EjecucionNegocio
+    {
+        private readonly EjecucionDao _dao;
+        private RespuestaModel m;
+
+        public EjecucionNegocio(IConfiguration config)
+        {
+            _dao = new EjecucionDao(config);
+            m = new RespuestaModel();
+        }
+
+        public EjecucionNegocio(IConfiguration config, string con)
+        {
+            _dao = new EjecucionDao(config, con);
+            m = new RespuestaModel();
+        }
+
+        public async Task<RespuestaModel> ObtenerGraficoEjecuciones(Dictionary<string, dynamic> P)
+        {
+            try
+            {
+                var res = await _dao.Consultar<GraficoModel>(P);
+                m.Datos = res;
+                m.Satisfactorio = true;
+                m.Id = 0;
+                m.Mensaje = "";
+                m.ErrorId = 0;
+            }
+            catch (Exception ex)
+            {
+                m.Datos = null;
+                m.Satisfactorio = false;
+                m.Id = 0;
+                m.Mensaje = "No se pudo realizar la solicitud. " + ex.Message + ". " + ex.InnerException;
+                m.ErrorId = 500;
+            }
+            return m;
+        }
+    }
+}
