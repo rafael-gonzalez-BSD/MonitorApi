02842aba82417c52a4a6d5dbc766bc5d7478706f
Rafael Gonzalez <gonzalezrg2@gmail.com>
2020-02-07 13:57:44 -0600

    Se sube complemento de excepcion estatus

---
+++ MonitorProcesos.Datos/Implementacion/ExcepcionEstatusDao.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using MonitorProcesos.Datos.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonitorProcesos.Datos.Implementacion
+{
+    public class ExcepcionEstatusDao: Disposable
+    {
+        internal DBConnection _db;
+
+        public ExcepcionEstatusDao(IConfiguration config)
+        {
+            _db = new DBConnection(config);
+        }
+
+        public ExcepcionEstatusDao(IConfiguration config, string con)
+        {
+            _db = new DBConnection(config, con);
+        }
+
+        public async Task<IEnumerable<T>> Consultar<T>(Dictionary<string, dynamic> P)
+        {
+            return await _db.QueryAsync<T>(P, "[Bitacora].[spExcepcionEstatus_Consultar]");
+        }
+    }
+}
---
+++ MonitorProcesos.Negocio/ExcepcionEstatusNegocio.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.Configuration;
+using MonitorProcesos.Datos.Base;
+using MonitorProcesos.Datos.Implementacion;
+using MonitorProcesos.Entidad.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonitorProcesos.Negocio
+{
+    public class ExcepcionEstatusNegocio: Disposable
+    {
+        private readonly ExcepcionEstatusDao _dao;
+        private RespuestaModel m;
+
+        public ExcepcionEstatusNegocio(IConfiguration config)
+        {
+            _dao = new ExcepcionEstatusDao(config);
+            m = new RespuestaModel();
+        }
+
+        public ExcepcionEstatusNegocio(IConfiguration config, string con)
+        {
+            _dao = new ExcepcionEstatusDao(config, con);
+            m = new RespuestaModel();
+        }
+
+        public async Task<RespuestaModel> ObtenerComboEsxcepcionEstatus(Dictionary<string, dynamic> P)
+        {
+            try
+            {
+                var res = await _dao.Consultar<ComboModel>(P);
+                m.Datos = res;
+                m.Satisfactorio = true;
+                m.Id = 0;
+                m.Mensaje = "";
+                m.ErrorId = 0;
+            }
+            catch (Exception ex)
+            {
+                m.Datos = null;
+                m.Satisfactorio = false;
+                m.Id = 0;
+                m.Mensaje = "No se pudo realizar la solicitud. " + ex.Message + ". " + ex.InnerException;
+                m.ErrorId = 500;
+            }
+            return m;
+        }
+    }
+}
