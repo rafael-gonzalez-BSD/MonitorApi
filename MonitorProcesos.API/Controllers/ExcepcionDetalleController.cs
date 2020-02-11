19239f18a556dc762b6a7a903319c67c18c7b2c1
Rafael Gonzalez <gonzalezrg2@gmail.com>
2020-02-11 08:50:25 -0600

    Aretefactos para Excepcion Detalle

---
+++ MonitorProcesos.API/Controllers/ExcepcionDetalleController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MonitorProcesos.Entidad.Base;
+using MonitorProcesos.Negocio;
+
+namespace MonitorProcesos.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExcepcionDetalleController : ControllerBase
+    {
+        private readonly ExcepcionDetalleNegocio n;
+
+        public ExcepcionDetalleController(IConfiguration config)
+        {
+            n = new ExcepcionDetalleNegocio(config, "MonitorRemoteDev");
+        }
+
+        [HttpGet("all")]
+        public async Task<RespuestaModel> ObtenerExcepcionDetalles(bool? Baja, int ExcepcionDetalleId = 0, int ExcepcionId = 0, int Opcion = 4)
+        {
+            Dictionary<string, dynamic> param = new Dictionary<string, dynamic>()
+            {
+                {"Opcion", Opcion },
+                {"ExcepcionDetalleId", ExcepcionDetalleId },
+                { "ExcepcionId", ExcepcionId },
+                { "Baja", Baja }
+        };
+            return await n.ObtenerExcepcionDetalles(param);
+        }
+    }
+}
---
+++ MonitorProcesos.Datos/Implementacion/ExcepcionDetalleDao.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using MonitorProcesos.Datos.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonitorProcesos.Datos.Implementacion
+{
+    public class ExcepcionDetalleDao : Disposable
+    {
+        internal DBConnection _db;
+
+        public ExcepcionDetalleDao(IConfiguration config)
+        {
+            _db = new DBConnection(config);
+        }
+
+        public ExcepcionDetalleDao(IConfiguration config, string con)
+        {
+            _db = new DBConnection(config, con);
+        }
+
+        public async Task<IEnumerable<T>> Consultar<T>(Dictionary<string, dynamic> P)
+        {
+            return await _db.QueryAsync<T>(P, "[Bitacora].[spExcepcionDetalle_Consultar]");
+        }
+    }
+}
