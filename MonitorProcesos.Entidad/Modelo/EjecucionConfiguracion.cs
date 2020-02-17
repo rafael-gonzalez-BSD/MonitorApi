472992c69486a2706a0a16f4905079419cbf0d30
Rafael Gonzalez <gonzalezrg2@gmail.com>
2020-02-17 16:09:48 -0600

    Modificacion de propiedades EjecucionConfiguracion

--- MonitorProcesos.Entidad/Modelo/EjecucionConfiguracion.cs
+++ MonitorProcesos.Entidad/Modelo/EjecucionConfiguracion.cs
@@ -5,43 +5,29 @@ namespace MonitorProcesos.Entidad.Modelo
     public class EjecucionConfiguracion
     {
         public int EjecucionConfiguracionId { get; set; }
-
         public short Frecuencia { get; set; }
-
         public TimeSpan HoraDesde { get; set; }
-
         public TimeSpan HoraHasta { get; set; }
-
         public string RutaLog { get; set; }
-
         public short TiempoEstimadoEjecucion { get; set; }
-
         public short TiempoOptimoEjecucion { get; set; }
-
         public int UsuarioCreacionId { get; set; }
-
         public DateTime FechaCreacion { get; set; }
-
         public int UsuarioModificacionId { get; set; }
-
         public DateTime FechaModificacion { get; set; }
-
         public int? UsuarioBajaId { get; set; }
-
         public DateTime? FechaBaja { get; set; }
-
         public bool Baja { get; set; }
-
         public int SistemaId { get; set; }
-
         public string SistemaDescripcion { get; set; }
-
         public int ProcesoId { get; set; }
-
         public string ProcesoDescripcion { get; set; }
-
         public string VentanaMantenimiento { get; set; }
-
+        public DateTime FechaDesdeVentana { get; set; }
+        public TimeSpan HoraDesdeVentana { get; set; }
+        public DateTime FechaHastaVentana { get; set; }
+        public TimeSpan HoraHastaVentana { get; set; }
+        public bool BajaVentana { get; set; }
         public int Opcion { get; set; }
     }
 }
