2ce3a75e955ac0db3ba411ea356917575827844e
Rafael Gonzalez <gonzalezrg2@gmail.com>
2020-02-25 12:08:28 -0600

    Ajuste en propieades nulas de conector

--- MonitorProcesos.Entidad/Modelo/Conector.cs
+++ MonitorProcesos.Entidad/Modelo/Conector.cs
@@ -13,8 +13,8 @@ namespace MonitorProcesos.Entidad.Modelo
         public int SistemaId { get; set; }
         public string SistemaDescripcion { get; set; }
         public string ConectorConfiguracionDescripcion { get; set; }
-        public int? Alertas { get; set; }
-        public int? Errores { get; set; }
+        public int Alertas { get; set; }
+        public int Errores { get; set; }
         public int? Eventos { get; set; }
     }
 }
